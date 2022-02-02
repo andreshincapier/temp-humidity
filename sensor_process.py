bf7583070b6addb1acda827b477aa9c09192b890
andreshincapier <andreshincapie82132@gmail.com>
2022-02-01 21:55:13 -0500

    Feature: Create documentation

--- sensor_process.py
+++ sensor_process.py
@@ -1,4 +1,5 @@
 import Adafruit_DHT
+import time
 
 pinGPIO = 4
 sensor = Adafruit_DHT.AM2302
@@ -9,8 +10,7 @@ def main():
         humidity, temperature = Adafruit_DHT.read_retry(sensor, pinGPIO)
 
         if humidity is not None and temperature is not None:
-            print(
-                "Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+            print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), temperature, humidity))
         else:
             print("Failed to retrieve data from humidity sensor")
 
